70918e5f7d445d1658f0b797fc06ac0a15c67215
Michał Zając <michal.t.zajac@gmail.com>
2023-10-02 17:46:06 +0200

    Weapon sheet - WIP

--- src/module/sheet/item/item-sheet.js
+++ src/module/sheet/item/item-sheet.js
@@ -5,15 +5,8 @@ export default class MiniSixItemSheet extends ItemSheet {
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
       classes: [SYSTEM_ID, 'sheet', 'item'],
-      tabs: [
-        {
-          navSelector: '.sheet-tabs',
-          contentSelector: '.sheet-body',
-          initial: 'details',
-        },
-      ],
-      width: 600,
-      height: 400,
+      width: 500,
+      height: 'auto',
     });
   }
 
@@ -31,17 +24,33 @@ export default class MiniSixItemSheet extends ItemSheet {
     const system = this.item.system;
     const sheetDetails = `systems/mini-six/templates/item/parts/${this.item.type}-details.hbs`;
     const itemDescription = await TextEditor.enrichHTML(system.description);
-    const attributes = (this.item.type === 'skill' && MiniSixItemSheet.prepareAttributes()) || undefined;
+    const attributes = MiniSixItemSheet.prepareAttributes();
     const newData = {
       system,
       sheetDetails,
       itemDescription,
       attributes,
+      skillList: this.skillList,
     };
     return logObject('Item Sheet Prepared object', foundry.utils.mergeObject(context, newData));
   }
 
+  get title() {
+    const itemTypeName = game.i18n.localize(`TYPES.Item.${this.item.type}`);
+    return `${itemTypeName}: ${this.item.name}`;
+  }
+
   get template() {
     return `systems/${SYSTEM_ID}/templates/item/item-sheet.hbs`;
+  }
+
+  get skillList() {
+    const onlyUnique = (value, index, array) => array.indexOf(value) === index;
+
+    const gameSkills = [...game.items.filter((item) => item.type === 'skill')].map((item) => item.name);
+    const parentItems = this.item.parent?.items ?? [];
+    const parentSkills = [...parentItems.filter((item) => item.type === 'skill').map((item) => item.name)];
+
+    return gameSkills.concat(parentSkills).filter(onlyUnique).sort();
   }
 }
